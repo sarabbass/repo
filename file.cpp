f4501ce5dca711b55cb259fd6b275f63c974a4e3
sara <sasoabdullahabbas@gmail.com>
2023-12-16 02:24:26 +0200

    this is the sum1

--- file.cpp
+++ file.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b);
+int sum1(int a, int b);
 
 int main()
 {
@@ -10,7 +10,7 @@ int main()
 	return 0;
 }
 
-int sum(int a, int b)
+int sum1(int a, int b)
 {
 
 	return a + b;
