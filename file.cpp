95abb9c0ae9ad4eab1eb10dc2429f58a92546489
sara <sasoabdullahabbas@gmail.com>
2023-12-16 02:23:35 +0200

    this is the sum in master

---
+++ file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b);
+
+int main()
+{
+	cout << "this is the first version" << endl;
+
+	return 0;
+}
+
+int sum(int a, int b)
+{
+
+	return a + b;
+}
